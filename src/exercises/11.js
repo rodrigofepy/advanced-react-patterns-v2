8a46263c7caac48eb5663b8605525ad4a97fb884
Rodrigo Ferreira <rodrigofepy@gmail.com>
2019-01-28 11:53:12 -0300

    Solution Exercise 11

--- src/exercises/11.js
+++ src/exercises/11.js
@@ -3,21 +3,25 @@ import React, {Fragment} from 'react'
 import {Switch} from '../switch'
 
 // 🐨 create your React context here with React.createContext
+const ToggleContext = React.createContext()
 
 class Toggle extends React.Component {
   // 🐨 expose the ToggleContext.Consumer as a static property of Toggle here.
-  state = {on: false}
+  static Consumer = ToggleContext.Consumer
+
   toggle = () =>
     this.setState(
       ({on}) => ({on: !on}),
       () => this.props.onToggle(this.state.on),
     )
+  state = {on: false, toggle: this.toggle}
   render() {
     // 🐨 replace this with rendering the ToggleContext.Provider
-    return this.props.children({
-      on: this.state.on,
-      toggle: this.toggle,
-    })
+    return (
+      <ToggleContext.Provider value={this.state}>
+        {this.props.children}
+      </ToggleContext.Provider>
+    )
   }
 }
 
